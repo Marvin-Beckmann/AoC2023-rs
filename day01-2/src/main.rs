ef5321268578641606bb2e6efcfdfcdbc69b9b35
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-12-07 17:18:30 +0100

    add solves for day 1

--- day01-2/src/main.rs
+++ day01-2/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+pub const DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+pub(crate) fn main() {
+    let mut puzzle_input = fs::read_to_string("src/puzzle_input.txt").unwrap();
+    for (index, string) in DIGITS.iter().enumerate() {
+        puzzle_input = puzzle_input.replace(string, &format!("{string}{}{string}", index + 1));
+    }
+    let mut sum = 0;
+    for c in puzzle_input.split('\n') {
+        let filtered: Vec<char> = c.chars().filter(|x| x.is_ascii_digit()).collect();
+        let first = filtered.first().unwrap();
+        let last = filtered.last().unwrap();
+        let int: u32 = format!("{first}{last}").parse().unwrap();
+        sum += int
+    }
+    print!("{}", sum);
 }
