428bae6b9fa1a493999f21c085ef18a9af05acca
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-12-13 08:10:44 +0100

    add solutions day 6

--- day06-1/src/main.rs
+++ day06-1/src/main.rs
@@ -1,3 +1,35 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("puzzle_input.txt").unwrap();
+    let (time, distance) = puzzle_input.split_once('\n').unwrap();
+    let time: Vec<usize> = time
+        .strip_prefix("Time:")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let distance: Vec<usize> = distance
+        .strip_prefix("Distance:")
+        .unwrap()
+        .split_ascii_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let total_winning_options: usize = (0..time.len())
+        .map(|race| compute_winning_options(time[race], distance[race]))
+        .product();
+    println!("{}", total_winning_options)
+}
+
+pub fn compute_winning_options(time: usize, distance: usize) -> usize {
+    let mut count = 0;
+    for charge_time in 1..time {
+        // compute distance based on charged time
+        let distance_after_charge = (time - charge_time) * charge_time;
+        if distance_after_charge > distance {
+            count += 1
+        }
+    }
+    count
 }
--- day06-2/src/main.rs
+++ day06-2/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("puzzle_input.txt").unwrap();
+    let (time, distance) = puzzle_input.split_once('\n').unwrap();
+    let time: usize = time
+        .strip_prefix("Time:")
+        .unwrap()
+        .replace(' ', "")
+        .parse()
+        .unwrap();
+    let distance: usize = distance
+        .strip_prefix("Distance:")
+        .unwrap()
+        .replace(' ', "")
+        .parse()
+        .unwrap();
+    let (lower, upper) = compute_roots_quadratic_function(time, distance);
+
+    println!("{}", upper.floor() - lower.ceil() + 1.0);
+}
+
+/// 'charge_time*(time - charge_time) - distance -1'
+/// = '-charge_time^2 + time*charge_time - distance -1'
+/// = '- (charge_time^2 - time*charge_time + distance + 1)'
+pub fn compute_roots_quadratic_function(time: usize, distance: usize) -> (f64, f64) {
+    let discriminant = ((time * time - 4 * (distance + 1)) as f64).sqrt();
+    let lower = ((time as f64 - discriminant) / 2.0).ceil();
+    let upper = ((time as f64 + discriminant) / 2.0).floor();
+
+    (lower, upper)
 }
