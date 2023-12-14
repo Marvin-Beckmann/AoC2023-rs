b3939fbe1580e83038a549c29426a3ccd667bbd0
Marvin Beckmann <marvin17@mail.uni-paderborn.de>
2023-12-14 16:56:10 +0100

    add solutions for day 09

--- day09-2/src/main.rs
+++ day09-2/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = fs::read_to_string("puzzle_input.txt").unwrap();
+    let res: i64 = puzzle_input
+        .lines()
+        .map(|x| {
+            x.split_whitespace()
+                .rev()
+                .map(|x| x.parse().unwrap())
+                .collect()
+        })
+        .map(predict_next)
+        .sum();
+    println!("{}", res)
+}
+
+pub fn predict_next(numbers: Vec<i64>) -> i64 {
+    if numbers.iter().all(|x| x == &0) {
+        0
+    } else {
+        let new_numbers: Vec<i64> = (0..(numbers.len() - 1))
+            .map(|x| numbers[x + 1] - numbers[x])
+            .collect();
+        numbers[numbers.len() - 1] + predict_next(new_numbers)
+    }
 }
